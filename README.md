675f29164050ed956e6e4325649273968bd7c118
Kiran Parajuli <kiran.parajuli@asians.cloud>
2025-02-18 12:14:46 +0545

    Login cust. with social providers
    Signed-off-by: Kiran Parajuli <kiran.parajuli@asians.cloud>

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ This will start a KeyCloak server at `http://localhost:8080`.
 ## Customization
 The theme is located in `./theme` directory. You can customize the theme by modifying the files in this directory.
 
+## Preview
+![Login Page](./Screenshot.png)
+
 ## References
 - [KeyCloak Documentation](https://www.keycloak.org/docs/latest/server_development/#_themes)
 - [KeyCloak Login FTL](https://github.com/keycloak/keycloak/blob/main/themes/src/main/resources/theme/base/login/login.ftl)
--- themes/my-custom-theme/login/resources/js/scripts.js
+++ themes/my-custom-theme/login/resources/js/scripts.js
@@ -1,16 +1,20 @@
 const introSection = document.createElement("section")
 introSection.innerHTML = "<h1>Sign in.</h1>"
-introSection.innerHTML += "<h3>Centralized Console</h3>"
+introSection.innerHTML += "<h3>Centralized Console <i class=\"fa fa-heartbeat\"></i></h3>"
 introSection.classList.add("kc-intro-section")
+
+// Add Font Awesome CSS
+const link = document.createElement("link")
+link.rel = "stylesheet"
+link.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+document.head.appendChild(link)
 
 window.onload = function() {
   document.body.insertBefore(introSection, document.body.firstChild)
 
   const header = document.querySelector("header.kc-form-header")
   header.querySelector("h1").outerHTML = `<h5>${header.querySelector("h1").innerHTML}</h5>`
-  const logo = document.createElement("img")
-  logo.src = "https://picsum.photos/120/60"
-  logo.alt = "Logo"
+  const logo = document.createElement("div")
   logo.classList.add("kc-branding")
   header.insertBefore(logo, header.firstChild)
 }
